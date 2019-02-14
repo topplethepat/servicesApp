4ce900b742595e8ec9bf5e977d89ad3ea7b88fd3
Lenore Alford <lenore.alford@gmail.com>
2019-02-14 09:22:33 -0800

    added dummy user to solve empty row issue

--- populate_svcs.py
+++ populate_svcs.py
@@ -19,46 +19,50 @@ DBSession = sessionmaker(bind=engine)
 # session.rollback()
 session = DBSession()
 
-# Menu for UrbanBurger
-service1 = Service(name="Parent Help")
+user1 = User(name="Townes", email="townes@gmail.com", picture='https://example.com')
+
+session.add(user1)
+session.commit()
+
+service1 = Service(user_id=1, name="Parent Help")
 
 session.add(service1)
 session.commit()
 
-taskItem2 = TaskItem(name="Babysitting ages 6 months and up", animal="human child", description="Play games, read books, prepare simple meal",
+taskItem2 = TaskItem(user_id=1, name="Babysitting ages 6 months and up", animal="human child", description="Play games, read books, prepare simple meal",
                      price="9 dollars per hour per child", service=service1)
 
 session.add(taskItem2)
 session.commit()
 
-taskItem3 = TaskItem(name="Parent helper ages 2-up", animal="human child", description="Play games, read books, make crafts in Canon Village while parent is home",
+taskItem3 = TaskItem(user_id=1,name="Parent helper ages 2-up", animal="human child", description="Play games, read books, make crafts in Canon Village while parent is home",
                      price="4 dollars per hour per child", service=service1)
 
 session.add(taskItem3)
 session.commit()
 
-service2 = Service(name="Cat care")
+service2 = Service(user_id=1, name="Cat care")
 
 session.add(service2)
 session.commit()
 
-taskItem4 = TaskItem(name="Catsitting", animal = "cat", description = "Feed, play with, snuggle", 
+taskItem4 = TaskItem(user_id=1, name="Catsitting", animal = "cat", description = "Feed, play with, snuggle", 
 							price = "10 dollars per visit", service = service2) 
 
 session.add(taskItem4)
 session.commit()
 
-service3 = Service(name="Dogwalking and care")
+service3 = Service(user_id=1, name="Dogwalking and care")
 
 session.add(service3)
 session.commit()
 
-taskItem5 = TaskItem(name="Dogwalking", animal = "dog", description = "Give your dog a brisk walk lasting 30 minutes", 
+taskItem5 = TaskItem(user_id=1, name="Dogwalking", animal = "dog", description = "Give your dog a brisk walk lasting 30 minutes", 
 							price = "10 dollars per walk", service = service3)
 session.add(taskItem5)
 session.commit()
 
-taskItem6 = TaskItem(name="Dogsitting", animal = "dog", description = "Feed, play with, snuggle for 30 minutes", 
+taskItem6 = TaskItem(user_id=1, name="Dogsitting", animal = "dog", description = "Feed, play with, snuggle for 30 minutes", 
 							price = "10 dollars per visit", service = service3)
 
 session.add(taskItem6)
